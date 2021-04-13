1345a6c4f5826ce3c2c4b6ee30ac4432dfe786c8
CalvinWan <s0610078@my.cmsh.cyc.edu.tw>
2021-04-13 09:59:18 +0800

    Update P-1-1. 合成函數(1).cpp

--- P-1-1. 合成函數(1)/P-1-1. 合成函數(1).cpp
+++ P-1-1. 合成函數(1)/P-1-1. 合成函數(1).cpp
@@ -1,1 +1,26 @@
-hello world
+#include <bits/stdc++.h>
+int fun()
+{
+	int x, y;
+	char input[10];
+	scanf("%s", input);
+	if (input[0] == 'f')
+	{
+		x = fun();
+		return 2 * x - 1;
+	}
+	else if (input[0] == 'g')
+	{
+		x = fun();
+		y = fun();f
+		return x + 2 * y - 3;
+	}
+	else
+		return atoi(input);
+}
+int main()
+{
+	printf("%d\n", fun());
+	system("PAUSE");
+	return 0;
+}
