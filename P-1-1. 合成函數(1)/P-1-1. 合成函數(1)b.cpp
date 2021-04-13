f9810f7cc92ea4df7ed7829067523a3b138c05cc
CalvinWan <s0610078@my.cmsh.cyc.edu.tw>
2021-04-13 10:13:41 +0800

    Update P-1-1. 合成函數(1)b.cpp

--- P-1-1. 合成函數(1)/P-1-1. 合成函數(1)b.cpp
+++ P-1-1. 合成函數(1)/P-1-1. 合成函數(1)b.cpp
@@ -1,1 +1,27 @@
-haluo
+#include <bits/stdc++.h>
+int fun()
+{
+	int val, x, y;
+	char a;
+	if (scanf("%d", &val) == 1)
+	{
+		return val;
+	}
+	scanf("%c", &a);
+	if (a == 'f')
+	{
+		x = fun();
+		return 2 * x - 1;
+	}
+	else if (a == 'g')
+	{
+		x = fun();
+		y = fun();
+		return x + 2 * y - 3;
+	}
+}
+int main()
+{
+	printf("%d\n", fun());
+	return 0;
+}
